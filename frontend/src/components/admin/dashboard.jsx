0efe9f2b9f6a2a814bd68f75f7a591f5284961fe
Rustam Ali <rustam@cwebconsultants.com>
2025-09-25 22:50:25 +0530

    Fix admin main page url click blink issue

--- frontend/src/components/admin/dashboard.jsx
+++ frontend/src/components/admin/dashboard.jsx
@@ -14,19 +14,19 @@ const Dashboard = () => {
           <div className="d-flex justify-content-between mt-5 pb-3">
             <h4 className="h4 pb-0 mb-el">Dashboard</h4>
           </div>
-          <div className="col-md-3 sidebar">
-            <div className="card shadow mb-5">
-              <div className="card-body p-4">
-                <ul>
-                <li><a href="">Dashboard</a></li>
-                <li><a href="">Categories</a></li>
-                <li><a href="">Brands</a></li>
-                <li><a href="">Products</a></li>
-                <li><a href="" onClick={logout}>Logout</a></li>
-                </ul>
+            <div className="col-md-3 sidebar">
+              <div className="card shadow mb-5">
+                <div className="card-body p-4">
+                  <ul>
+                  <li><a href="" onClick={(e) => e.preventDefault()}>Dashboard</a></li>
+                  <li><a href="" onClick={(e) => e.preventDefault()}>Categories</a></li>
+                  <li><a href="" onClick={(e) => e.preventDefault()}>Brands</a></li>
+                  <li><a href="" onClick={(e) => e.preventDefault()}>Products</a></li>
+                  <li><a href="" onClick={(e) => { e.preventDefault(); logout();}}>Logout</a></li>
+                  </ul>
+                </div>
               </div>
             </div>
-          </div>
           <div className="col-md-9">
             {/* Add content for right column here */}
           </div>
